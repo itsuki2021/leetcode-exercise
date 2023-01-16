b96103b799a51cb534ceeb26436990a31c9201a4
lst <shitong_liu@163.com>
2023-01-16 16:52:16 +0800

    add str.h str.cpp (kmp algorithm)

---
+++ include/str.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace kmp {
+
+using std::string;
+using std::vector;
+
+class KMP {
+public:
+    static int search(const string& haystack, const string& needle);
+private:
+    static vector<int> getNext(const string s);
+};
+
+
+
+};  // namespace kmp
--- main.cpp
+++ main.cpp
@@ -1,30 +1,57 @@
 #include "utils.hpp"
 #include "tree.h"
+#include "binary_search.hpp"
 #include <iostream>
 #include <algorithm>
-#include <cassert>
 
 using namespace std;
 
 
 class Solution {
 public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) {
-        return false;
+    vector<int> getNext(const string& s) {
+        vector<int> next(s.size());
+        int j = -1;
+        next[0] = j;
+        for (int i = 1; i < s.size(); ++i) {
+            while (j >= 0 && s[i] != s[j + 1]) j = next[j];
+
+            if (s[i] == s[j + 1]) ++j;
+
+            next[i] = j;
+        }
+
+        return next;
+    }
+
+    int strStr(string haystack, string needle) {
+        if (needle.size() == 0) return 0;
+
+        vector<int> next = getNext(needle);
+        int j = -1;
+        for (int i = 0; i < haystack.size(); ++i) {
+            while (j >= 0 && haystack[i] != needle[j + 1]) j = next[j];
+            if (haystack[i] == needle[j + 1]) ++j;
+
+            if (j == (needle.size() - 1)) return i - needle.size() + 1;
+        }
+
+        return -1;
     }
 };
 
-
 int main() {
-    // vector<int> nums = {3522,181,521,515,304,123,2512,312,922,407,146,1932,4037,2646,3871,269};
-    // int k = 5;
-    // auto sol = new Solution();
-    // auto ans = sol->canPartitionKSubsets(nums, k);
-    // cout << "\nAnswer is:\n";
-    // cout << ans << endl;
-    // // utils::printVec1D(ans);
-    // // utils::printVec2D(ans);  
-    // delete sol;
+    string haystack = "aabaabaafa";
+    string needle = "aabaaf";
+    // string needle = "abcabdddabcabc";
+
+    auto sol = new Solution();
+    auto ans = sol->strStr(haystack, needle);
+    cout << "\nAnswer is:\n";
+    cout << ans << endl;
+    // utils::printVec1D(ans);
+    // utils::printVec2D(ans);
+    delete sol;
 
     return 0;
 }
