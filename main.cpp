4e1756b7518e5859978b25034f0bcbf6ad168ca5
lst <shitong_liu@163.com>
2023-08-15 15:13:15 +0800

    add vim files

--- main.cpp
+++ main.cpp
@@ -1,52 +1,49 @@
-#include "utils.hpp"
-#include "tree.h"
-#include "binary_search.hpp"
 #include <iostream>
 #include <algorithm>
+#include <stack>
+
+#include "utils.hpp"
+#include "tree.h"
 
 using namespace std;
+using bin_tree::TreeNode;
+using bin_tree::NULL_NODE;
 
-
-class Solution {
+class Solution
+{
 public:
-    vector<int> getNext(const string& s) {
-        vector<int> next(s.size());
-        int j = -1;
-        next[0] = j;
-        for (int i = 1; i < s.size(); ++i) {
-            while (j >= 0 && s[i] != s[j + 1]) j = next[j];
+    vector<int> argsort(const vector<int>& vec) {
+        vector<int> res;
+        for (int i = 0; i < vec.size(); ++i) res.push_back(i);
 
-            if (s[i] == s[j + 1]) ++j;
+        sort(res.begin(), res.end(), 
+            [&](int a, int b) { return vec[a] < vec[b]; });
 
-            next[i] = j;
-        }
-
-        return next;
+        return res;
     }
 
-    int strStr(string haystack, string needle) {
-        if (needle.size() == 0) return 0;
-
-        vector<int> next = getNext(needle);
-        int j = -1;
-        for (int i = 0; i < haystack.size(); ++i) {
-            while (j >= 0 && haystack[i] != needle[j + 1]) j = next[j];
-            if (haystack[i] == needle[j + 1]) ++j;
-
-            if (j == (needle.size() - 1)) return i - needle.size() + 1;
+    string findReplaceString(string s, vector<int> &indices,
+                             vector<string> &sources, vector<string> &targets) {
+        int offset = 0, minInd = 0;
+        for (int i : argsort(indices)) {
+            if (s.substr(indices[i] + offset, sources[i].size()) == sources[i]) {
+                s.replace(indices[i] + offset, sources[i].size(), targets[i]);
+                offset += targets[i].size() - sources[i].size();
+            }
         }
 
-        return -1;
+        return s;
     }
 };
 
 int main() {
-    string haystack = "aabaabaafa";
-    string needle = "aabaaf";
-    // string needle = "abcabdddabcabc";
+    string s = "vmokgggqzp";
+    vector<int> indices = {3, 5, 1};
+    vector<string> sources = {"kg","ggq","mo"};
+    vector<string> target = {"s","so","bfr"};
 
     auto sol = new Solution();
-    auto ans = sol->strStr(haystack, needle);
+    auto ans = sol->findReplaceString(s, indices, sources, target);
     cout << "\nAnswer is:\n";
     cout << ans << endl;
     // utils::printVec1D(ans);
